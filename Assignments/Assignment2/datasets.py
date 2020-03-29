b735b5fd96bc534da316a97abb0245b92223976d
Nalet Meinen <nalet.meinen@outlook.com>
2020-03-29 22:58:22 +0200

    update with Assignment 2 and Tutorials

---
+++ Assignments/Assignment2/datasets.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from PIL import Image
+import glob
+
+import torch.utils.data as data
+
+class ImageNetLimited(data.Dataset):
+    """ImageNet Limited dataset."""
+    
+    def __init__(self, root_dir, transform=None):
+        
+        pass
+
+    def __len__(self):
+        
+        pass
+    
+        return 1
+
+    def __getitem__(self, idx):
+        
+        pass
+        
