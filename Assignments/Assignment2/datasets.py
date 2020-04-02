dea0efcb4f41ef891aca01f39131a342ead3e737
Nalet Meinen <nalet.meinen@outlook.com>
2020-04-02 09:59:23 +0200

    Assignment 2 data loaded

--- Assignments/Assignment2/datasets.py
+++ Assignments/Assignment2/datasets.py
@@ -1,23 +1,22 @@
 from __future__ import print_function
 from PIL import Image
 import glob
-
+from tqdm.notebook import tqdm
 import torch.utils.data as data
 
 class ImageNetLimited(data.Dataset):
     """ImageNet Limited dataset."""
     
     def __init__(self, root_dir, transform=None):
-        
-        pass
+        data = []
+        for sample in tqdm(root_dir):
+            data.append(sample)
+        self.n = len(root_dir)
+        self.data = data
 
     def __len__(self):
-        
-        pass
-    
-        return 1
+        return self.data[ind]
 
     def __getitem__(self, idx):
-        
-        pass
+        return self.n
         
